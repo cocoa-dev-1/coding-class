265aa96deeaafcfac60acbe397e48226fb378427
yargeu <yargeu@gmail.com>
2021-05-20 20:00:04 +0900

    fixed 나누기.py

--- 나누기.py
+++ 나누기.py
@@ -2,7 +2,7 @@ mainInt = int(input())
 sumInt = int(input())
 
 result1 = mainInt // 100 * 100
-result2 = result1 + (sumInt-(result1 % sumInt))
+result2 = result1 + (sumInt-(result1 % sumInt)) if result1 % sumInt != 0 else result1
 finalResult = result2 % 100
 if finalResult < 10:
     finalResult = '0{0}'.format(finalResult)
