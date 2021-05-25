8d106c9d64cc633a95a2a000d1929645e463f9d6
sin532100 <63899067+sin532100@users.noreply.github.com>
2021-05-25 23:33:41 +0900

    저항, 사이클-단어, 문자의-합 file has added, updated

--- 문자의-합.py
+++ 문자의-합.py
@@ -1,3 +1,18 @@
+dic = {}
+
 for _ in range(int(input())):
-    s = input()
+  dic = {}
+  for i in range(65,91):
+    dic[chr(i)] = 0
+
+  s = input()
+  for i in s:
+    if i in dic:
+      dic[i] += 1 
+  
+  for a, b in list(dic.items()):
+    if b == 0:
+      print(ord(a))
+  
+
     
--- 사이클-단어.py
+++ 사이클-단어.py
@@ -1,4 +1,18 @@
-word = ['']
+woord_list = []
 
 for i in range(int(input())):
-  word.append(input())
+  result2 = True
+  a = input()
+  woord_list.append(a) if i == 0 else None
+  for j in range(len(a)):
+    result = a[j:]+a[:j]
+    if result in woord_list:
+      result2 = True
+      break
+    else:
+      result2 = False
+  
+  if result2 == False:
+    woord_list.append(a)
+    
+print(len(woord_list))
