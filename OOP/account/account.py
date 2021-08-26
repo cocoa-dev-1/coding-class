2e8af0ac8124bddceb81e949dae0a811aedb0160
cocoa-dev-1 <sinmounbu0510@gmail.com>
2021-08-26 17:03:20 +0900

    2021-08-26 account loan

---
+++ OOP/account/account.py
@@ -0,0 +1,98 @@
+import uuid
+
+class Account:
+    def __init__(self):
+        self._id = uuid.uuid1()
+        self._balance = 0
+        self._rates = 0
+
+    def __str__(self):
+        return f"{self._id} : ${self._balance}"
+
+
+class Customer:
+    def __init__(self, name, age, gender, income):
+        self.name = name
+        self.age = age
+        self.gender = gender
+        self.income = income
+        self.accounts = []
+
+    def add_account(self, account):
+        self.accounts.append(account)
+    
+    def __str__(self):
+        return f"{self.name}({self.age}, {self.gender}) : ${self.income}"
+
+    def display_account(self):
+        for acc in self.accounts:
+            print(acc.__str__)
+
+
+class Loan:
+    OVERDUE_PENALTY = 0.5
+    PAYMENT_DAY = 15
+    
+    def __init__(self):
+        self.debt = 0.0
+        self.month = 0
+        self.loan_interest = 1.0
+        
+    def loan(self, amount, month):
+        self.debt += amount
+        self.month = month
+    
+    def repay(self, amount):
+        self.debt -= amount
+        return self.debt
+    
+    def get_payment(self):
+        return int(self.debt * (1+self.loan_interest)/self.month)
+
+    def __str__(self):
+        return f"total debt : {self.debt}({self.month} month)"
+
+    def adjust_interest(self, interest):
+        self.loan_interest = interest
+    
+    def overdue_penalty(self, penalty):
+        self.debt += self.debt * self.OVERDUE_PENALTY
+        return self.debt
+
+
+class Banking:
+    def __init__(self):
+        self.interest = 1.0
+    
+    def deposit(self):
+        pass
+
+    def withdraw(self):
+        pass
+
+
+class Bank(Banking, Loan):
+    def __init__(self):
+        Banking.__init__(self)
+        Loan.__init__(self)
+        self.__accounts = []
+
+
+class LoanAccount(Account, Bank):
+    def __init__(self, owner):
+        Account.__init__(self)
+        owner.add_account(Account)
+
+    def deposit(self, amount):
+        self._balance += amount
+        if self.debt > 0:
+            payment = self.get_payment()
+            print('You have to repay $',payment)
+            self._balance -= payment
+            self.repay(payment)
+
+    def withdraw(self, amount):
+        self._balance -= amount
+
+    def __str__(self):
+        pass
